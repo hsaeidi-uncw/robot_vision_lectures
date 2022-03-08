fc8d4f5e4d30c5aa20581affd6464462b21e0b9d
hsaeidi-uncw <saeidih@uncw.edu>
2022-03-08 06:36:06 -0500

    pcl filter working

--- src/crop_3D_image.cpp
+++ src/crop_3D_image.cpp
@@ -47,11 +47,30 @@ void get_pcl(const sensor_msgs::PointCloud2Ptr& cloud){
 
 	// convert the ROS message to PCL
 	pcl::fromROSMsg(*cloud, raw_pcl);
-
+	// raise the flag
 	pcl_received = true;
 }
 
+pcl::PointCloud<pcl::PointXYZ> crop_pcl(pcl::PointCloud<pcl::PointXYZ>& _raw_pcl, cv::Mat& filtered_2D_img){
+	// define an empty point cloud
+	pcl::PointCloud<pcl::PointXYZ> tmp_pcl;
+	// a 3D point for filtering the ball image
+	pcl::PointXYZ point_buffer_usedInAtMethod; 
+	// loop through the rows and columns
+	for (int row = 0; row < filtered_2D_img.rows; row++){
+		for (int col = 0; col < filtered_2D_img.cols; col++){
+			// if there is a pixel on the ball (from the ball_2D topic add the corresponding point to the point cloud)
+			if (filtered_2D_img.at<uchar>(row,col) == 255){
+				point_buffer_usedInAtMethod = _raw_pcl.at(col , row);
+				tmp_pcl.push_back(point_buffer_usedInAtMethod);
+			}
+		
+		}
+		
+	}
+	return tmp_pcl;
 
+}
 
 int main(int argc, char * argv[]){
 	ros::init(argc,argv,"crop_3D_image");
@@ -68,15 +87,15 @@ int main(int argc, char * argv[]){
 	
 	int seq = 0;
 	while(ros::ok()){
-		if (pcl_received){
+		if (pcl_received && image_received){
 			pcl::PointCloud<pcl::PointXYZ> cropped_pcl;
-
-			cropped_pcl = raw_pcl;
+			// crop the pointcloud
+			cropped_pcl = crop_pcl(raw_pcl, img);
+			//update the message and publish it
 			std_msgs::Header header;
 			header.stamp = ros::Time::now();
 			header.seq = seq++;
-			header.frame_id = std::string( "camera_depth_optical_frame" );
-		
+			header.frame_id = std::string( "camera_color_optical_frame" );
 			cropped_pcl.header = pcl_conversions::toPCL( header );
 			pub_pcl.publish( cropped_pcl );
 		}
