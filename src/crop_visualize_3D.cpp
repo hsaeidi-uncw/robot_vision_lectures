4bca48571228b72035de5c657edfb20bcf78463d
hsaeidi-uncw <saeidih@uncw.edu>
2022-03-10 06:18:49 -0500

    sphere visualization

--- src/crop_visualize_3D.cpp
+++ src/crop_visualize_3D.cpp
@@ -8,7 +8,10 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
 #include <robot_vision_lectures/XYZarray.h>
+#include <robot_vision_lectures/SphereParams.h>
 #include <geometry_msgs/Point.h>
+
+#include <cmath>
 
 using namespace cv;
 using namespace std;
@@ -51,6 +54,14 @@ void get_pcl(const sensor_msgs::PointCloud2Ptr& cloud){
 	pcl::fromROSMsg(*cloud, raw_pcl);
 	// raise the flag
 	pcl_received = true;
+}
+
+robot_vision_lectures::SphereParams sphere_params;
+
+void get_sphere_params(const robot_vision_lectures::SphereParams& _params){
+	//read the parameters and save them
+	sphere_params = _params;
+	std::cout << sphere_params.xc << " " << sphere_params.yc << " " << sphere_params.zc << " "<< sphere_params.radius << std::endl;
 }
 
 pcl::PointCloud<pcl::PointXYZ> crop_pcl(pcl::PointCloud<pcl::PointXYZ>& _raw_pcl, cv::Mat& filtered_2D_img){
@@ -97,6 +108,32 @@ robot_vision_lectures::XYZarray convert_pcl(pcl::PointCloud<pcl::PointXYZ>& _pcl
 }
 
 
+pcl::PointCloud<pcl::PointXYZ> visualize_sphere(const robot_vision_lectures::SphereParams& _params){
+	// define an empty point cloud
+	pcl::PointCloud<pcl::PointXYZ> tmp_pcl;
+	pcl::PointXYZ tmp_point;
+	// using the spherical coordinates to produce xyz using the coordinates of the center and the radius
+	// example of math: https://tutorial.math.lamar.edu/classes/calciii/SphericalCoords.aspx
+	// break the theta and phi values to 20 chunks between 0 and 2*Pi
+	int theta_segments = 20;
+	int phi_segments = 20;
+	for (int theta_ind = 0; theta_ind < theta_segments; theta_ind++){
+		for (int phi_ind = 0; phi_ind < phi_segments; phi_ind++){
+			// calculate current theta and phi on the grid
+			float theta = (float) theta_ind/theta_segments*2*M_PI;
+			float phi = (float) phi_ind/phi_segments*2*M_PI;
+			// use the param values and update the points
+			tmp_point.x = _params.xc + _params.radius*sin(phi)*cos(theta);
+			tmp_point.y = _params.yc + _params.radius*sin(phi)*sin(theta);
+			tmp_point.z = _params.zc + _params.radius*cos(phi); 
+			tmp_pcl.push_back(tmp_point);
+		}
+		
+	}
+	return tmp_pcl;
+
+}
+
 
 int main(int argc, char * argv[]){
 	ros::init(argc,argv,"crop_3D_image");
@@ -105,12 +142,15 @@ int main(int argc, char * argv[]){
 	ros::Subscriber cam_sub = nh_.subscribe("/ball_2D", 1, get_img);
 	// subscriber for reading the registered 3D point cloud
 	ros::Subscriber pcl_sub = nh_.subscribe("/camera/depth_registered/points",1,get_pcl);
-	
+
+	// subscriber for reading shpere fit parameters (x, y, z) of the centery and the radius
+	ros::Subscriber sphere_sub = nh_.subscribe("/sphere_params",1, get_sphere_params);
+		
 	// publisher for writing the cropped pcl
 	ros::Publisher pub_pcl= nh_.advertise<sensor_msgs::PointCloud2>( "/pcl_cropped_ball", 1 );
 	
 	// publisher for writing the ball fit pcl
-	ros::Publisher pub_fit= nh_.advertise<sensor_msgs::PointCloud2>( "/ball_2D_fit", 1 );
+	ros::Publisher pub_fit= nh_.advertise<sensor_msgs::PointCloud2>( "/ball_3D_fit", 1 );
 	
 	// publisher for writing the cropped xyz
 	ros::Publisher pub_xyz= nh_.advertise<robot_vision_lectures::XYZarray>( "/xyz_cropped_ball", 1 );
@@ -129,10 +169,14 @@ int main(int argc, char * argv[]){
 
 	while(ros::ok()){
 		if (pcl_received && image_received){
+			//clear the current pcl
 			cropped_pcl.clear();
-			ball_fit_pcl.clear();	
 			// crop the pointcloud
 			cropped_pcl = crop_pcl(raw_pcl, img);
+			//clear the current pcl
+			ball_fit_pcl.clear();
+			// update the fit based on the parameters
+			ball_fit_pcl = visualize_sphere(sphere_params);				
 			//update the message and publish it
 			std_msgs::Header header;
 			header.stamp = ros::Time::now();
@@ -140,6 +184,8 @@ int main(int argc, char * argv[]){
 			header.frame_id = std::string( "camera_color_optical_frame" );
 			cropped_pcl.header = pcl_conversions::toPCL( header );
 			pub_pcl.publish( cropped_pcl );
+			ball_fit_pcl.header = pcl_conversions::toPCL( header );
+			pub_fit.publish( ball_fit_pcl );
 			// update the xyz array and publish it
 			xyz.points.clear();
 			xyz = convert_pcl(cropped_pcl);
